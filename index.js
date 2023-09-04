8b7753e5000111b55b34e838c79ed6fe508be358
Kaleb Bolack <kalebbolack14@gmail.com>
2023-09-03 20:06:08 -0600

    Completed all test's!

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const myElement = document.getElementById('button');
+  
+    function myEventListenerFunction(event) {
+      console.log('Event occurred:', event);
+    }
+  
+    myElement.addEventListener('click', myEventListenerFunction);
+  }
+  addingEventListener();
+  
